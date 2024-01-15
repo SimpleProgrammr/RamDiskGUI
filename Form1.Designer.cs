90ae61e19327d1c418b8e3816c2790345bc63e15
Progrmm <michal.pelon@proton.me>
2024-01-15 10:28:56 +0100

    Final

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,6 +38,13 @@ namespace RamDiskGUI
             mountButton = new Button();
             label4 = new Label();
             label3 = new Label();
+            label5 = new Label();
+            diskNameTextBox = new TextBox();
+            fileSystemComboBox = new ComboBox();
+            label2 = new Label();
+            enableCompressionCheckBox = new CheckBox();
+            clusterSizeComboBox = new ComboBox();
+            label6 = new Label();
             ((System.ComponentModel.ISupportInitialize)diskSpaceNumeric).BeginInit();
             SuspendLayout();
             // 
@@ -55,11 +62,11 @@ namespace RamDiskGUI
             // diskSpaceNumeric
             // 
             diskSpaceNumeric.Font = new Font("Segoe UI", 10F);
-            diskSpaceNumeric.Location = new Point(95, 104);
+            diskSpaceNumeric.Location = new Point(95, 188);
             diskSpaceNumeric.Maximum = new decimal(new int[] { 4096, 0, 0, 0 });
             diskSpaceNumeric.Minimum = new decimal(new int[] { 10, 0, 0, 0 });
             diskSpaceNumeric.Name = "diskSpaceNumeric";
-            diskSpaceNumeric.Size = new Size(120, 25);
+            diskSpaceNumeric.Size = new Size(121, 25);
             diskSpaceNumeric.TabIndex = 1;
             diskSpaceNumeric.Value = new decimal(new int[] { 10, 0, 0, 0 });
             // 
@@ -67,15 +74,15 @@ namespace RamDiskGUI
             // 
             diskLetterComboBox.Font = new Font("Segoe UI", 10F);
             diskLetterComboBox.FormattingEnabled = true;
-            diskLetterComboBox.Location = new Point(96, 48);
+            diskLetterComboBox.Location = new Point(95, 99);
             diskLetterComboBox.Name = "diskLetterComboBox";
-            diskLetterComboBox.Size = new Size(121, 25);
+            diskLetterComboBox.Size = new Size(120, 25);
             diskLetterComboBox.TabIndex = 5;
             // 
             // mountButton
             // 
             mountButton.Font = new Font("Segoe UI", 15F);
-            mountButton.Location = new Point(36, 135);
+            mountButton.Location = new Point(35, 305);
             mountButton.Name = "mountButton";
             mountButton.Size = new Size(152, 58);
             mountButton.TabIndex = 6;
@@ -87,7 +94,7 @@ namespace RamDiskGUI
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 10F);
-            label4.Location = new Point(12, 106);
+            label4.Location = new Point(12, 190);
             label4.Name = "label4";
             label4.Size = new Size(77, 19);
             label4.TabIndex = 9;
@@ -97,17 +104,93 @@ namespace RamDiskGUI
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 10F);
-            label3.Location = new Point(12, 51);
+            label3.Location = new Point(12, 102);
             label3.Name = "label3";
             label3.Size = new Size(78, 19);
             label3.TabIndex = 8;
             label3.Text = "Disk Letter:";
             // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 10F);
+            label5.Location = new Point(12, 145);
+            label5.Name = "label5";
+            label5.Size = new Size(80, 19);
+            label5.TabIndex = 11;
+            label5.Text = "File System:";
+            // 
+            // diskNameTextBox
+            // 
+            diskNameTextBox.Font = new Font("Segoe UI", 10F);
+            diskNameTextBox.Location = new Point(12, 54);
+            diskNameTextBox.Name = "diskNameTextBox";
+            diskNameTextBox.PlaceholderText = "Ram Disk";
+            diskNameTextBox.Size = new Size(203, 25);
+            diskNameTextBox.TabIndex = 12;
+            diskNameTextBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // fileSystemComboBox
+            // 
+            fileSystemComboBox.Font = new Font("Segoe UI", 10F);
+            fileSystemComboBox.FormattingEnabled = true;
+            fileSystemComboBox.Location = new Point(95, 142);
+            fileSystemComboBox.Name = "fileSystemComboBox";
+            fileSystemComboBox.Size = new Size(120, 25);
+            fileSystemComboBox.TabIndex = 13;
+            fileSystemComboBox.SelectedIndexChanged += FileSystemComboBox_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10F);
+            label2.Location = new Point(11, 271);
+            label2.Name = "label2";
+            label2.Size = new Size(133, 19);
+            label2.TabIndex = 14;
+            label2.Text = "Enable compression:";
+            // 
+            // enableCompressionCheckBox
+            // 
+            enableCompressionCheckBox.AutoSize = true;
+            enableCompressionCheckBox.Font = new Font("Segoe UI", 11F);
+            enableCompressionCheckBox.Location = new Point(172, 276);
+            enableCompressionCheckBox.Name = "enableCompressionCheckBox";
+            enableCompressionCheckBox.Size = new Size(15, 14);
+            enableCompressionCheckBox.TabIndex = 15;
+            enableCompressionCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // clusterSizeComboBox
+            // 
+            clusterSizeComboBox.Font = new Font("Segoe UI", 10F);
+            clusterSizeComboBox.FormattingEnabled = true;
+            clusterSizeComboBox.Location = new Point(94, 230);
+            clusterSizeComboBox.Name = "clusterSizeComboBox";
+            clusterSizeComboBox.Size = new Size(120, 25);
+            clusterSizeComboBox.TabIndex = 17;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 10F);
+            label6.Location = new Point(12, 233);
+            label6.Name = "label6";
+            label6.Size = new Size(82, 19);
+            label6.TabIndex = 16;
+            label6.Text = "Cluster Size:";
+            // 
             // mainWindow
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(228, 201);
+            ClientSize = new Size(228, 374);
+            Controls.Add(clusterSizeComboBox);
+            Controls.Add(label6);
+            Controls.Add(enableCompressionCheckBox);
+            Controls.Add(label2);
+            Controls.Add(fileSystemComboBox);
+            Controls.Add(diskNameTextBox);
+            Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(mountButton);
@@ -117,6 +200,7 @@ namespace RamDiskGUI
             Name = "mainWindow";
             ShowIcon = false;
             Text = "Ram Disk Mount";
+            FormClosing += mainWindow_FormClosing;
             Load += MainWindow_Load;
             ((System.ComponentModel.ISupportInitialize)diskSpaceNumeric).EndInit();
             ResumeLayout(false);
@@ -131,5 +215,12 @@ namespace RamDiskGUI
         private System.Windows.Forms.Button mountButton;
         private Label label4;
         private Label label3;
+        private Label label5;
+        private TextBox diskNameTextBox;
+        private ComboBox fileSystemComboBox;
+        private Label label2;
+        private CheckBox enableCompressionCheckBox;
+        private ComboBox clusterSizeComboBox;
+        private Label label6;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 using RamDisk;
+using System.Diagnostics;
 
 namespace RamDiskGUI
 {
@@ -11,7 +12,7 @@ namespace RamDiskGUI
 
         private void MainWindow_Load(object sender, EventArgs e)
         {
-            List<string> letters = new() { "A:\\", "B:\\", "C:\\", "D:\\", "E:\\", "F:\\", "G:\\", "H:\\", "I:\\", "J:\\", "K:\\", "L:\\", "M:\\", "N:\\", "O:\\", "P:\\", "Q:\\", "R:\\", "S:\\", "T:\\", "U:\\", "V:\\", "W:\\", "X:\\", "Y:\\", "Z:\\" };
+            List<string> letters = ["A:\\", "B:\\", "C:\\", "D:\\", "E:\\", "F:\\", "G:\\", "H:\\", "I:\\", "J:\\", "K:\\", "L:\\", "M:\\", "N:\\", "O:\\", "P:\\", "Q:\\", "R:\\", "S:\\", "T:\\", "U:\\", "V:\\", "W:\\", "X:\\", "Y:\\", "Z:\\"];
             DriveInfo[] drives = DriveInfo.GetDrives();
             foreach (DriveInfo drive in drives)
             {
@@ -27,6 +28,20 @@ namespace RamDiskGUI
             diskSpaceNumeric.Maximum = MaxSpace;
             diskSpaceNumeric.Value = MaxSpace / 4;
 
+            fileSystemComboBox.Items.Clear();
+            fileSystemComboBox.Items.Add("NTFS");
+            fileSystemComboBox.Items.Add("FAT32");
+            fileSystemComboBox.Items.Add("FAT");
+            fileSystemComboBox.Items.Add("exFAT");
+            fileSystemComboBox.SelectedIndex = 0;
+
+            clusterSizeComboBox.Items.Clear();
+            string[] clusterSize = ["512", "1024", "2048", "4096", "8192", "16K", "32K", "64K", "128K", "256K", "512K", "1M", "2M"];
+            foreach (var cs in clusterSize)
+            {
+                clusterSizeComboBox.Items.Add(cs);
+            }
+            clusterSizeComboBox.SelectedItem = "4096";
         }
 
 
@@ -34,15 +49,65 @@ namespace RamDiskGUI
         {
             int space = (int)diskSpaceNumeric.Value;
             char letter = (char)diskLetterComboBox.SelectedItem.ToString()[0];
-            string name = "RamDrive";
+            string name = diskNameTextBox.Text == string.Empty ? diskNameTextBox.PlaceholderText : diskNameTextBox.Text;
+            var fs = FileSystem.NTFS;
+            int clusterSize = 4096;
+            bool enableCompression = false, quickFormat = true;
+
+
+            switch (fileSystemComboBox.SelectedItem.ToString())
+            {
+                case "NTFS":
+                    fs = FileSystem.NTFS;
+                    break;
+                case "FAT32":
+                    fs = FileSystem.FAT32;
+                    break;
+                case "FAT":
+                    fs = FileSystem.FAT;
+                    break;
+                case "exFAT":
+                    fs = FileSystem.exFAT;
+                    break;
+            }
+
+
             if (mountButton.Text == "Mount")
             {
+                RamDrive.Mount(space, fs, letter, name);
+
+                string fs1 = fileSystemComboBox.SelectedItem.ToString();
+                try
+                {
+                    var di = new DriveInfo(letter + ":");
+                    var psi = new ProcessStartInfo
+                    {
+                        FileName = "format.com",
+                        CreateNoWindow = true, //if you want to hide the window
+                        WorkingDirectory = Environment.SystemDirectory,
+                        Arguments = "/FS:" + fs + " /Y" + " /V:" + name.Replace(' ', '_') + (quickFormat ? " /Q" : "") + ((fs1 == "NTFS" && enableCompression) ? " /C" : "") + " /A:" + clusterSize + " " + letter + ":",
+                        UseShellExecute = false
+                    };
+                    psi.CreateNoWindow = true;
+                    psi.RedirectStandardOutput = true;
+                    psi.RedirectStandardInput = true;
+                    var formatProcess = Process.Start(psi);
+                    var swStandardInput = formatProcess.StandardInput;
+                    swStandardInput.WriteLine();
+                    formatProcess.WaitForExit();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                    return;
+                }
+
+                mountButton.Text = "Unmount";
                 diskLetterComboBox.Enabled = false;
                 diskSpaceNumeric.Enabled = false;
-                
-                RamDrive.Mount(space, FileSystem.NTFS, letter, name);
-                 
-                mountButton.Text = "Unmount";
+                diskNameTextBox.Enabled = false;
+                fileSystemComboBox.Enabled = false;
             }
             else
             {
@@ -50,10 +115,26 @@ namespace RamDiskGUI
                 mountButton.Text = "Mount";
                 diskLetterComboBox.Enabled = true;
                 diskSpaceNumeric.Enabled = true;
+                diskNameTextBox.Enabled = true;
+                fileSystemComboBox.Enabled = true;
 
             }
-            
+
         }
 
+        private void FileSystemComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (fileSystemComboBox.SelectedItem.ToString() != "NTFS")
+            {
+                enableCompressionCheckBox.Checked = false;
+                enableCompressionCheckBox.Enabled = false;
+            }
+            enableCompressionCheckBox.Enabled = true;
+        }
+
+        private void mainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Mount(sender, e);
+        }
     }
 }
